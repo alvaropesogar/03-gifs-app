893c78ff9b4221b59d3eed0e5146140cfec9bd49
Álvaro Peso García <alvaropeso@proton.me>
2026-06-29 10:12:53 +0200

    Sepración bloques navegación + funcionalidad navegación

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string;
+  sublabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.html',
+})
+export class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line',
+    },
+
+    {
+      label: 'Search',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass',
+    },
+  ];
+}
